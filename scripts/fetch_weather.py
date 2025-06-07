08396f880af59bbba378ebeab09c59a4f3046cb1
Pergih <pedromo5555@gmail.com>
2025-06-07 15:07:53 +0100

    added: validation

--- scripts/fetch_weather.py
+++ scripts/fetch_weather.py
@@ -34,6 +34,24 @@ def get_today_weather():
     else:
         print("Error on", response.status_code)
 
+def validate_weather_data(data: dict) -> bool:
+    required_fields = ['api_timestamp', 'location', 'temp_c', 'humidity', 'precip_mm', 'wind_kph']
+    for field in required_fields:
+        if field not in data or data[field] is None:
+            return False
+
+    if not (-100 < data['temp_c'] < 100):
+        print("Error on temp_c")
+        return False
+    if not (0 <= data['precip_mm'] <= 1):
+        print("Error on precip_mm")
+        return False
+    if not (0 <= data['wind_kph']):
+        print("Error on wind_kph")
+        return False
+
+    return True
+
 def generate_mock_sales(temp_c, precip_mm):
     umbrellas = random.randint(10, 30) if precip_mm > 1 else random.randint(0, 10)
     cold_drinks = random.randint(20, 50) if temp_c > 20 else random.randint(5, 15)
@@ -50,10 +68,14 @@ def run_pipeline():
     sales = generate_mock_sales(weather['temp_c'], weather['precip_mm'])
 
     # Insert data
+    if validate_weather_data(weather) == False:
+        print("Pipeline run failed.")
+        # log it
+        return
     add_weather(**weather)
     add_sale(weather['date'], sales['store_id'], sales['umbrellas'], sales['cold_drinks'])
 
-    print("✅ Pipeline run completed.")
+    print("Pipeline run completed.")
 
     
 if __name__ == "__main__":
